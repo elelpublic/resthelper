3c9f5b750f9840b486462811deb59de5a706fe4b
larsfrommars <larsfrommars@gmail.com>
2014-12-10 11:59:35 +0100

    0.0.9
    Override the default config base dir with the
    „com.infodesire.resthelper.configBaseDir“ system property

--- src/main/java/com/infodesire/resthelper/BaseDir.java
+++ src/main/java/com/infodesire/resthelper/BaseDir.java
@@ -12,24 +12,33 @@ import java.io.File;
  * Directory which contains sub directories for each app.
  * <p>
  * 
- * The default for the base dir is ~/.bsmapps 
+ * The default for the base dir is ~/.bsmapps
+ * <p> 
+ * 
+ * The default base dir can be overridden with the com.infodesire.resthelper.configBaseDir
+ * system property. 
  *
  */
 public class BaseDir {
 
   
   /**
-   * @param baseDirName Name of the base dir or null for defaul
+   * @param baseDirName Name of the base dir or null for default
    * @return Directory which contains sub directories for each app
    * 
    */
   public static File getBaseDir( String baseDirName ) {
     
-    if( Strings.isNullOrEmpty( baseDirName ) ) {
-      baseDirName = new File( System.getProperty( "user.home", "." ) )
-        .getAbsolutePath() + "/.bsmapps";
+    if( !Strings.isNullOrEmpty( baseDirName ) ) {
+      return new File( baseDirName );
     }
-    return new File( baseDirName );
+    
+    String property = System.getProperty( "com.infodesire.resthelper.configBaseDir" );
+    if( Strings.isNullOrEmpty( property ) ) {
+      return new File( property );
+    }
+    
+    return new File( new File( System.getProperty( "user.home", "." ) ), ".bsmapps" );
   }
   
 
